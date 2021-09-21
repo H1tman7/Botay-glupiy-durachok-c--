6a63c5b194320726fe75452917ac9d853474189d
Mikhail <87526852+H1tman7@users.noreply.github.com>
2021-09-22 01:06:50 +0500

    Added third and fourth tasks

--- Homework3.cpp
+++ Homework3.cpp
@@ -1,6 +1,12 @@
-#define _USE_MATH_DEFINES
-#include <cmath>
 #include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <stdbool.h>
+// #include <map>
+// #include <functional>
+// using dict_fun_t = map<int, function<void()>>;
+
 
 using namespace std;
 
@@ -12,12 +18,10 @@ int end() {
     return num_task;
 }
 
-
-void Number1(){
+void Number1() {
     cout << "Number 1" << endl;
 
-
-    float S,r,p,n;
+    float S, p, n;
     cout << "Enter percent: ";
     cin >> p;
     cout << "Enter sum: ";
@@ -25,23 +29,20 @@ void Number1(){
     cout << "Enter count of ages: ";
     cin >> n;
 
-    if (S <= 0 || n <= 0.003 || p < -100 || p > 100){
+    if (S <= 0 || n <= 0.003 || p < -100 || p > 100) {
         cout << "Error input!" << endl;
-        end();
         return;
     }
 
-    r = p / 100;
-    cout << "m = " << ( S * r * pow((1 + r), n) ) / ( 12 * ( pow(1 + r, n) - 1) ) << endl;
+    cout << "Monthly payment = " << (S * p / 100 * pow((1 + p / 100), n)) / (12 * (pow(1 + p / 100, n) - 1)) << endl;
 
-    end();
     return;
 }
 
-void Number2(){
+void Number2() {
     cout << "Number 2" << endl;
 
-    float S, r, m, n;
+    float S, m, n, monthly, ans = INFINITY, closest = 10e10;
 
     cout << "Enter sum: ";
     cin >> S;
@@ -52,80 +53,183 @@ void Number2(){
     cout << "Enter monthly payment: ";
     cin >> m;
 
+    if (S < 0 || n < 0.003 || m < 0) {
+        cout << "Input Error!" << endl;
+        end();
+        return;
+    }
 
+    for (float p = -100; p <= 100; p += 0.01) {
+        monthly = (S * (p / 100) * pow((1 + (p / 100)), n)) / (12 * (pow((1 + (p / 100)), n) - 1));
+        if (abs(m - monthly) < closest) {
+            closest = abs(m - monthly);
+            ans = p;
+        }
+    }
 
-
+    cout << "Answer = " << ans << " %" << endl;
     return;
 
 }
 
+void Number3() {
+    cout << "Number 3" << endl;
+    ofstream fout("The File.txt"); // Объект выходного потока
+
+    fout << "Hello!";
+    cout << "Enter something:" << endl;
+    char str[255];
+
+    while (fgets(str,255,stdin)) {
+        fout << str;
+    }
+
+    cout << "Ok, I wrote down this information in the file. You can check this one!\n" << endl;
+    fout.close();
+
+    ifstream fin("The File.txt");
+    cout << "This is what contains in this file:" << endl;
+    cout << "-----------------------------------------------------" << endl;
+    char ch;
+    while (fin.get(ch)) {
+        cout << ch;
+    }
+    cout << "-----------------------------------------------------" << endl;
+    cout << "The end.\n" << endl;
+
+    fin.close();
+
+    return;
+}
+
+void Number4(){
+    cout << "Number 4" << endl;
+
+    ifstream fin("Task 4.txt");
+    char c;
+    bool flag = false, sp = false;
+
+    while (fin.get(c)){
+        if (c >= '0' && c <= '9'){
+            cout << c;
+            flag = true;
+            sp = false;
+        }
+        if (c == '\n'){
+            cout << endl;
+            flag = false;
+        }
+        else if (!flag){
+            if (!sp){
+            cout << " ";
+            sp = true;
+            }
+        }
+        else{
+            flag = false;
+        }
+    }
+    printf("\n");
+    return;
+}
+
+/*
+void Number5(){
+    cout << "Number 5" << endl;
+    char string[30];
+
+    cout << "Enter 30-letters string: " << endl;
+
+    for(int i = 0; i < 30; i++){
+        cin >> string[i];
+    }
+
+    for(int i = 0; i < 30; i++){
+        cout << i+1 << "element = " << string[i] << endl;
+    }
+
+    std::sort();
+
+
+    return;
+}
+*/
+
 
 int main() {
     cout << "Homework 3" << endl;
-    end();
 
     while (true) {
+        end();
         switch (num_task) {
-            case 0:
-                cout << "Ok, programm is finished." << endl;
-                return 0;
-                
-            case 1:
-                Number1();
-                break;
+        case 0:
+            cout << "Ok, programm is finished." << endl;
+            return 0;
 
-            case 2:
-                Number2();
-                break;
+        case 1:
+            Number1();
+            break;
+
+        case 2:
+            Number2();
+            break;
+
+        case 3:
+            Number3();
+            break;
+
+        case 4:
+            Number4();
+            break;
 /*
-            case 3:
-                Number3();
-                break;
-
-            case 4:
-                Number4();
-                break;
-
-            case 5:
-                Number5();
-                break;
+        case 5:
+            Number5();
+            break;
 */
-            default:
-                cout << "Probably you entered an incorrect number. Try again!" << endl;
-                end();
-                break;
+        default:
+            cout << "Probably you entered an incorrect number. Try again!" << endl;
+            break;
         }
     }
     return 0;
 }
 
 
-
-
-
-
-
+/*
+5 task - русский алфавит. не использ. библиотеку. пузырек
+        нельзя вектора.... можно - циклы массивы, if-ы
+*/
 
 
 
 
 /*
+int main() {
+    cout << "Homework 3" << endl;
 
-1 задача - дата минимум 0,003 гоба(1 день)
-
-
-Ссуда - невозможно выразить процент; в цикле перебирать проценты -100 до 100; придумать способ быстрее(для плюса)
-        как можно продасть свою верность за 40 евро - гуглим.
-
-3 task - если создать файл из программы - убедиться, что можно добавить больше 1 строки( самый безопасный метод - просто открыть готовый файл)
-
-4 task - тестовый файл. не выводим буквы(если буквы - ставим пробелы)
-
-5 task - русский алфавит. не использ. библиотеку. пузырек
-        нельзя вектора.... можно - циклы массивы, if-ы
+    dict_fun_t dict = {
+        { 1, Number1 },
+        { 2, Number2 },
+    };
 
 
-конспект лекций
-136страница. - работа с файлами
+    while (1) {
+        char s[100];
+        fgets(s, 100, stdin);
+        if (s[0] == '0') {
+            cout << "Ok, programm is finished." << endl;
+            break;
+        }
 
+        int n = atoi(s);
+
+        if (n && dict.count(n)) {
+            dict[n]();
+        } else {
+            cout << "Probably you entered an incorrect number. Try again!" << endl;
+        }
+    }
+
+    return 0;
+}
 */
