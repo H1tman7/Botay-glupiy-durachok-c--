b8b7000034f5466f72535dff3ccd5fe2283e975c
Mikhail <87526852+H1tman7@users.noreply.github.com>
2021-12-01 11:55:10 +0300

    spinners done

--- spinners.cpp
+++ spinners.cpp
@@ -8,6 +8,106 @@ int end() {
     return num_task;
 }
 
+void Number1(){
+    unsigned long long a, b, c;
+    int ans = 0;
+    cin >> a >> b >> c;
+
+    c -= a;
+    while (c > b){
+        c -= b;
+        ans++;
+    }
+    cout << ans << endl;
+
+    return;
+}
+
+void Number2(){
+    unsigned long long m;
+    cout << "Enter M: ";
+    cin >> m;
+
+    if ((m - 4 * (m % 3)) / 3 >= 0 && m != 1 && m != 2 && m != 5)
+        cout << (m - 4 * (m % 3)) / 3 << endl << m % 3 << endl;
+    else
+        cout << 0 << endl << 0 << endl;
+
+    return;
+}
+
+void Number3(){
+
+    uint64_t n, m;
+    cout << "n: ";
+    cin >> n;
+    cout << "m: ";
+    cin >> m;
+
+    cout << ((n + 1) * (m + 1) * (n * m)) / 4 << endl;
+
+    return;
+}
+
+void Number4(){
+    int n, k, i = 0, ans = 0, max = 0;
+    cout << "enter N: ";
+    cin >> n;
+    int place[54 + 1] = { '\0' };
+    int del4[9] = { '\0' };
+
+    while (n > 0){
+        cin >> k;
+        place[k] = 1;
+        if (k % 4 == 0 && k < 37){
+            del4[i] = k;
+            i++;
+        }
+        n--;
+    }
+
+    sort(&del4[0], &del4[9]);
+    i = 8;
+    int prev = 100;
+
+    while(del4[i] != 0){
+        if (place[del4[i]] && place[del4[i] - 1] && place[del4[i] - 2] && place[del4[i] - 3]) {
+            if(place[54 - 2 * (del4[i] / 4 - 1)] && place[53 - 2 * (del4[i] / 4 - 1)]){
+                if(prev == 100){
+                    ans = 1;
+                    max = 1;
+                }
+                else if(abs(del4[i] - prev) == 4){
+                    ans += 1;
+                    if (ans > max)
+                        max = ans;
+                }
+                else
+                    ans = 1;
+            }
+        }
+        prev = del4[i];
+        i--;
+    }
+
+    cout << "ans = " << max << endl;
+
+    return;
+}
+
+void Number5(){
+    int N, K;
+    cin >> N >> K;
+
+    while (K != 1){
+        N = (N - K % 2) / 2;
+        K /= 2;
+    }
+    cout << (N - 1) / 2 << endl << N / 2 << endl;
+
+    return;
+}
+
 int main(){
     cout << "Spinners" << endl;
     while (true) {
@@ -43,73 +143,4 @@ int main(){
         }
     }
     return 0;
-}
-
-void Number1(){
-
-    return;
-}
-
-void Number2(){
-
-    return;
-}
-
-void Number3(){
-
-    return;
-}
-
-void Number4(){
-    int n, k, i = 0, ans = 0, max = 0;
-    cout << "enter N: ";
-    cin >> n;
-    int place[54 + 1] = { '\0' };
-    int del4[9] = { '\0' };
-    while (n > 0){
-        cin >> k;
-        place[k] = 1;
-        if (k % 4 == 0 && k < 37){
-            del4[i] = k;
-            i++;
-        }
-        n--;
-    }
-    sort(&del4[0], &del4[9]);
-    i = 8;
-    int prev = 100;
-    while(del4[i] != 0){
-        if (place[del4[i]] && place[del4[i] - 1] && place[del4[i] - 2] && place[del4[i] - 3]) {
-            if(place[54 - 2 * (del4[i] / 4 - 1)] && place[53 - 2 * (del4[i] / 4 - 1)]){
-                if(prev == 100){
-                    ans = 1;
-                    max = 1;
-                }
-                else if(abs(del4[i] - prev) == 4){
-                    ans += 1;
-                    if (ans > max)
-                        max = ans;
-                }
-                else
-                    ans = 1;
-            }
-        }
-        prev = del4[i];
-        i--;
-    }
-    cout << "ans = " << max << endl;
-    return;
-}
-
-void Number5(){
-    int N, K;
-    cin >> N >> K;
-
-    while (K != 1){
-        N = (N - K % 2) / 2;
-        K /= 2;
-    }
-    cout << (N - 1) / 2 << endl << N / 2 << endl;
-
-    return;
 }
